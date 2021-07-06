39ed079db496e4398352a7f0b981c3452d922379
Alex Germanov <germanov@a-group.aero>
2021-07-06 09:58:29 +0300

    Generator correction

--- generator.py
+++ generator.py
@@ -3,8 +3,38 @@ import hashlib
 
 # TODO 2. Написать генератор, который принимает путь к файлу. При каждой итерации возвращает md5 хеш каждой строки
 #  файла.
-def md5(line):
+def md5(line, encoding='utf-8'):
     hash_md5 = hashlib.md5()
-    hash_md5.update(line)
-    print(hash_md5.hexdigest())
+    hash_md5.update(line.encode(encoding))
     return hash_md5.hexdigest()
+path = 'country_url.txt'
+md5(path)
+
+def gen(path):
+    with open(path) as file:
+        for line in file:
+            yield line
+
+for item in gen(path):
+    print(md5(item))
+
+
+
+
+
+
+
+
+
+
+# def md5(line):
+#     hash_md5 = hashlib.md5()
+#     hash_md5.update(line)
+#     # print(hash_md5.hexdigest())
+#     return hash_md5.hexdigest()
+#
+#
+# def gen(path):
+#     with open(path) as file:
+#         for line in file:
+#             yield line.upper()
